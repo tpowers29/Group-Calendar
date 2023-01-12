bd6117f60e68da151120728b403db37c681615ac
tpowers29 <113735451+tpowers29@users.noreply.github.com>
2023-01-12 15:45:47 -0500

    Worked on models

--- Project-2/Main/models/index.js
+++ Project-2/Main/models/index.js
@@ -1,13 +1,6 @@
 const User = require('./User');
-const Project = require('./Project');
+const Event = require('./Event');
 
-User.hasMany(Project, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
 
-Project.belongsTo(User, {
-  foreignKey: 'user_id'
-});
 
-module.exports = { User, Project };
+module.exports = { User, Event };
