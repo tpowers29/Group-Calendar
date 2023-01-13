3e738ae79a5d5d8e52e02f7f16f731deeb2334ca
tpowers29 <113735451+tpowers29@users.noreply.github.com>
2023-01-12 21:36:15 -0500

    adding posts.js

---
+++ models/Posts.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+const User = require('./User');
+
+class Posts extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+Posts.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comment: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    reaction: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    user: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    community: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+
+    {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'posts',
+  }
+);
+
+module.exports = Posts;
